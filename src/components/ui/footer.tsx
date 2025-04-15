47a54a1d5441c5b4f57471b26a9d3801d03b70ac
haashirnawaz9 <153224457+haashirnawaz9@users.noreply.github.com>
2025-04-15 18:19:12 -0400

    Your commit message here

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,14 +1,8 @@
-"use client"
-import { InstagramIcon } from 'next-share'
-import Link from 'next/link'
 
 function Footer() {
   return (
     <div className="flex flex-row justify-center items-center text-center h-[60px]">
-            <h1 className="font-normal text-sm">Copyright © 2024 ImageGenAI - Haashir Nawaz | All Rights Reserved</h1>
-            <Link href="https://www.instagram.com/haashir_nawaz_/">      
-                <InstagramIcon size={34} round className="ml-3" />
-            </Link>
+            <h1 className="font-normal text-sm">Copyright © 2025 ImageGenAI | All Rights Reserved</h1>
     </div>
   )
 }
